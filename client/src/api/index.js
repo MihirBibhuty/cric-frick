782b92aea45d45b5cb452ba6316ea7b567802ea0
MihirBibhuty <bibhutymihir@gmail.com>
2023-05-21 00:55:08 +0530

    backend hosted

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://cric-frick.onrender.com' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
